5432cbac696b9c02d33582da3478f8a55497014d
Alexander B <bas2784@gmail.com>
2023-08-03 08:35:30 +0300

    move const

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import { ShopItem } from "./components/shop_item/ShopItemFC";
+import './components/css/main.css'
 
-export const App = (item) => {
+
+export const App = () => {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
   return (
     <div className="container">
       <div className="background-element">
--- src/components/shop_item/ShopItemFC.jsx
+++ src/components/shop_item/ShopItemFC.jsx
@@ -1,25 +1,15 @@
-import './css/main.css'
+export function ShopItem ({item}) {
 
-const item = {
-  brand: 'Tiger of Sweden',
-  title: 'Leonard coat',
-  description: 'Minimalistic coat in cotton-blend',
-  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-  price: 399,
-  currency: '£'
-}
-
-export const ShopItem = () => {
   return (
-    <div class="main-content">
+    <div className="main-content">
       <h2>{item.brand}</h2>
       <h1>{item.title}</h1>
       <h3>{item.description}</h3>
-      <div class="description">{item.descriptionFull}</div>
-      <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">{item.currency}{item.price.toFixed(2)}</div>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{item.currency}{item.price.toFixed(2)}</div>
         <button>Добавить в корзину</button>
       </div>
     </div>
